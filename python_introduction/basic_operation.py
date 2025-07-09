eab1fbef09544fe0f525cd54307cf4ace3151bae
alhagie nyang <alhagienyang13@gmail.com>
2025-07-09 15:28:59 +0000

    Python Basic Operations

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,12 @@
+# Python basic Operations
+number1 = 10
+number2 = 5
+# Addition Operations
+sum = number1 + number2
+print("Addition of ",number1 , "and", number2, "is", sum)
+#  Subtraction Operation
+result= number1- number2
+print("Subtraction of" , number1, "and" ,number2 , "is" , result)
+# the product operations of the two numbers
+product = number1 * number2
+print ("Multiplications of " , number1 , "and" , number2 , "is" , product)
