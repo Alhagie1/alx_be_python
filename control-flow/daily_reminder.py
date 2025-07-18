4720418ce254c55cf736b57d7846b8f9320f1209
alhagie nyang <alhagienyang13@gmail.com>
2025-07-18 18:50:24 +0000

    personal daily reminder program

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,16 +1,16 @@
 
 # Personal Daily Reminder program
 
-task = input("Describe your task:") # Task Description
+Task = input("What is your task? :") # Task Description
 
-priority = input("What is your task priority? (high, medium, low):").lower() # Task priority
+Priority = input("What is the priority level of your task (high, medium, low): ").lower() # Task priority
 
-time_bound = input("Is the task time-bound (yes/no):").lower() 
+Time_bound = input("Is your task time-boun?  (yes/no) :").lower() 
 
 priority_description = ""
 priority_is_valid = True
 
-match priority:
+match Priority:
     case "high":
         priority_description = "a high priority task"
     case "medium":
@@ -22,14 +22,13 @@ match priority:
         priority_is_valid = False
 
 if priority_is_valid:
-     if time_bound  == "yes":
-         print(f"Reminder: {task} is {priority_description} task that requires immediate attention")
-     elif time_bound == "no":
-         print(f"Note: {task} is a {priority_description}. Consider finishing it in your free time.")
+     if Time_bound  == "yes":
+         print(f"Reminder: {Task} is {priority_description} task that requires immediate attention")
+     elif Time_bound == "no":
+         print(f"Note: {Task} is a {priority_description}. Consider finishing it in your free time.")
      else:
-         print("Entere an invalid time bound just answer yes or no")
-else:
-  print(f"{priority_is_valid} is False")
+         print("Entered an invalid time bound just answer (yes or no) ")
+
 
 
 
