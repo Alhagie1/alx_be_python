da68fcae7f9d25e1074afa2ebaaa15fcccb48bdd
alhagie nyang <alhagienyang13@gmail.com>
2025-07-18 12:41:07 +0000

    Weather recommendation program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,15 @@
 
 # A Weather Recommendation Program
-weather_conditon = input("What is the weather like today? (sunny/rainy/cold):")
 
-if weather_conditon == "sunny":
+# Asking for user's weather input
+weather = input("What is the weather like today? (sunny/rainy/cold):")
+
+# Giving recommendation based on provided weather conditions
+if weather == "sunny":
     print("Wear a t-shirt and sun glasses")
-elif weather_conditon == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and raincoat")
-elif weather_conditon == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf")
 else:
     print("Sorry I don't have recommendation for this weather")
