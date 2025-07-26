342a91ff98d3db30af81fc0870f483f8b6689af6
alhagie nyang <alhagienyang13@gmail.com>
2025-07-26 03:49:31 +0000

    Temperature Conversion Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,25 +1,27 @@
 
 
-FARRENHEIT_TO_CELCIUS_FACTOR = 5/9
-CELCIUS_TO_FARENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELCIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-def convert_to_celcius(temp_in_farenheit):
-    """Temperature from farenheit to celcius"""
-    temp_in_celcius = (temp_in_farenheit - 32) * FARRENHEIT_TO_CELCIUS_FACTOR
-    return temp_in_celcius
-def convert_to_farenheit(temp_in_celcius):
-    """Temperature from celcius to farenheit"""
-    temp_in_farenheit = (temp_in_celcius * CELCIUS_TO_FARENHEIT_FACTOR) + 32
-    return temp_in_farenheit
+def convert_to_celsius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
+    """Temperature from farenheit to celsius"""
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
+    """Temperature from celsius to farenheit"""
+    fahrenheit = (celsius * CELCIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return fahrenheit
 
 temperature = float(input("Enter the temperature to convert: "))
-unit = input("Is this temperature in Celcius or Farenheit? (C/F): ").strip().upper()
+unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 
 if unit == "C":
-    my_temp = convert_to_farenheit(temperature)
+    my_temp = convert_to_fahrenheit(temperature)
     print(f"{temperature}°C is {my_temp}°F")
 elif unit == "F":
-    my_temp = convert_to_celcius(temperature)
+    my_temp = convert_to_celsius(temperature)
     print(f"{temperature}°F is {my_temp}°C")
 else:
     print("Invalid temperature input. Please enter C or F ")
