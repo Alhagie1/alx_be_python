b969c7b770242133021a5130c9d2ed6aa3e598ec
alhagie nyang <alhagienyang13@gmail.com>
2025-07-26 04:41:51 +0000

    Explore 'datetime' Module

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print(f"Current date and time: {current_date.strftime("%Y-%m-%d %H:%M:%S")}")
+    number_of_days = int(input("Enter number of days to add to current date: "))
+    def calculate_future_date(current_date, days_to_add):
+       future_date = current_date + datetime.timedelta(days = days_to_add)
+       return future_date  
+    final_future_date = calculate_future_date(current_date, number_of_days)
+    print(f"Future date: {final_future_date.strftime("%Y-%m-%d %H:%M:%S")}")
+display_current_datetime()
+    
