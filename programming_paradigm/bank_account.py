14ac5dd3a6f43acfc86095e599771ac8a3dcab52
alhagie nyang <alhagienyang13@gmail.com>
2025-08-02 15:53:34 +0000

    A Simple Bank Account class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,7 @@
 
 
 class BankAccount:
-    def __init__(self, account_balance = 0.0):
+    def __init__(self, account_balance = 0.00):
         self.account_balance = account_balance
     
 
@@ -17,7 +17,7 @@ class BankAccount:
             return False
     
     def display_balance(self):
-        print(f"Current Balance: ${float(self.account_balance)}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
     
 
    
